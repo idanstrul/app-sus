12cc796867c9dbe3ab302c4d29fe4c44fe9a8cff
idanstr <strulovich@mail.tau.ac.il>
2022-03-02 20:06:58 +0200

    Progress in note-filter

--- js/apps/keep/cmps/note-filter.cmp.js
+++ js/apps/keep/cmps/note-filter.cmp.js
@@ -1,7 +1,37 @@
 export default {
     template: `
     <section class="note-filter">
-        <h1>Note filter</h1>
+        <input type="text" v-model="filterBy.search">
     </section>
-    `
+    `,
+    data() {
+        return {
+            filterBy: {
+                search: '',
+                type: ''
+            }
+        }
+    },
+    methods: {
+        debounce(func, timeout = 300) {
+            let timer;
+            return (...args) => {
+                clearTimeout(timer);
+                timer = setTimeout(() => { func.apply(this, args); }, timeout);
+            };
+        },
+        setFilter() {
+            console.log('Setting Filter!');
+            this.$emit('filtered', this.filterBy)
+        },
+    },
+    watch: {
+        filterBy: {
+            handler() {
+                console.log('yeh');
+                this.debounce(this.setFilter)
+            },
+            deep: true
+        }
+    }
 }
